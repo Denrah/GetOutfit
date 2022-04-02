c069906eda874ea8a53623695c1e6d9a76746d1c
Denis Sharapov <denisharapov@yandex.ru>
2022-04-02 15:19:59 +0700

    Initial Commit

---
+++ GetOutfit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetOutfit
+//
+//  Created by National Team on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
